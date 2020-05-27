cc35b0294a28e663a00dc31fc937c70df3dc2b31
Matthew Haigh <mlhaigh@users.noreply.github.com>
2020-05-27 13:06:13 -0400

    updated name_imports to 7.5

--- name_imports.py
+++ name_imports.py
@@ -1,5 +1,5 @@
-for ea in range(0x416468, 0x41668C):
-    if 'off' in Name(ea):
-        addr = Dword(ea)   
-        new_name = Name(Dword(ea))
-        MakeName(ea, new_name+'_0')
+for ea in range(0x73116a2c, 0x73116aa8):
+    if 'off' in get_name(ea):
+        addr = get_wide_dword(ea)   
+        new_name = get_name(get_wide_dword(ea))
+        set_name(ea, new_name+'_0')
